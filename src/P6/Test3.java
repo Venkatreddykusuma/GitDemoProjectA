757207f3de7d23d4de3ef8ef3469972cbd08d9ae
Venkatreddykusuma <kusumavenkatreddy8@gmail.com>
2023-01-04 23:06:19 +0530

    changes test3 line

--- src/P6/Test3.java
+++ src/P6/Test3.java
@@ -2,8 +2,9 @@ package P6;
 
 public class Test3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Test3 is executed");
 
 	}
 
